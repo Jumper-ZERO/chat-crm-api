ad492eb5e25104ff4a559e8ec00d4ee1ad04614e
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-08-26 19:28:50 +0000

    refactor: :bulb: Drop commented code

--- src/config/cache.config.ts
+++ src/config/cache.config.ts
@@ -1,24 +1,3 @@
-// import { CacheModuleOptions } from '@nestjs/cache-manager';
-
-// import { createKeyv } from '@keyv/redis';
-// import { CacheableMemory } from 'cacheable';
-// import { Keyv } from 'keyv';
-
-// export const cacheConfig: CacheModuleOptions = {
-//   stores: [
-//     new Keyv({
-//       store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
-//     }),
-//     createKeyv(getRedisUrl()),
-//   ],
-// };
-
-// function getRedisUrl(): string {
-//   return process.env.REDIS_PASSWORD
-//     ? `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
-//     : `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
-// }
-
 import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
 
 import { createKeyv } from '@keyv/redis';
