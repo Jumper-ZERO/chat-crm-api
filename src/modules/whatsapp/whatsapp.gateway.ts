511601d3ebc398224234df1006bc675bc4f85d74
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-10-12 19:05:56 +0000

    feat: :fire: Delete duplicate methods

--- src/modules/whatsapp/whatsapp.gateway.ts
+++ src/modules/whatsapp/whatsapp.gateway.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { PinoLogger } from 'nestjs-pino';
 import { Server, Socket } from 'socket.io';
@@ -67,35 +65,5 @@ export class WhatsappGateway implements OnGatewayConnection, OnGatewayDisconnect
     this.logger.debug(`Mensaje guardado en DB: ${JSON.stringify(msg)}`);
 
     this.server.to(data.chat).emit('new-message', msg);
-  }
-
-  @SubscribeMessage('sendMessage')
-  handleMessage(@MessageBody() data: { to: string; text: string }) {
-    console.log('📨 Mensaje desde React:', data);
-
-    this.server.emit('message', {
-      from: 'me',
-      to: data.to,
-      text: data.text,
-      isSent: true,
-    });
-  }
-
-  notifyIncomingMessage(msg: any) {
-    this.server.emit('message', {
-      from: msg.from,
-      text: msg.text,
-      isSent: false,
-    });
-  }
-
-  @SubscribeMessage('joinChat')
-  handleJoinChat(@ConnectedSocket() client: Socket, @MessageBody() chatId: string) {
-    void client.join(chatId);
-    console.log(`Cliente ${client.id} unido al chat ${chatId}`);
-  }
-
-  emitIncomingMessage(chatId: string, message: any) {
-    this.server.to(chatId).emit('newMessage', message);
   }
 }
